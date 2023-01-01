2d062d5fdb64367ea64bf3ef17b5e4fae64cf7be
DTTerastar <DT@Terastar.biz>
2023-01-01 17:57:37 -0500

    Zoom added!

--- ClientApp/src/routes/+page.svelte
+++ ClientApp/src/routes/+page.svelte
@@ -4,6 +4,8 @@
   import { config, devices } from '../lib/stores';
   import type { Config, Device, Node, Room } from '../lib/types';
   import { scaleOrdinal, schemeCategory10 } from "d3";
+  import { select } from "d3-selection";
+  import { zoom } from 'd3-zoom';
   import { setContext } from 'svelte';
   import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
 
@@ -15,16 +17,26 @@
   import AxisY from './AxisY.svelte';
   import DeviceDetails from './DeviceDetails.svelte';
 
+  let svg:Element;
   const selected = writable<Device | null>();
   const hovered = writable<Device| null>();
   const floor = writable<number>(0);
 
+  function zoomed({ transform }) {
+    select(svg).attr("transform", transform);
+  }
+
+  const handler = zoom()
+    .scaleExtent([1, 40])
+    .on("zoom", zoomed);
+
+
   setContext('colors', scaleOrdinal(schemeCategory10))
 
-
+  $: { if (svg) select(svg).call(handler) }
   $: bounds = $config?.floors[$floor]?.bounds
 
-  function selecteda (d:Device | null) {
+  function detail (d:Device | null) {
 		$selected = d;
     drawerStore.open({id:"device"});
 	}
@@ -38,12 +50,12 @@
 <div class="w-full h-full">
   <FloorTabs selected={floor} />
   <LayerCake x='0' y='1' flatData={ bounds } xReverse={ false } yReverse={ true } padding={ {top: 0, left: 0, bottom: 16, right: 0} }>
-    <Svg>
+    <Svg bind:element={svg}>
       <AxisX />
       <AxisY />
       <Rooms floor={$floor} />
       <Nodes radarId={$hovered?.id ?? $selected?.id} floor={$floor} />
-      <Devices floor={$floor} on:selected={ d => selecteda(d.detail) } on:hovered={ d => $hovered = d.detail } />
+      <Devices floor={$floor} on:selected={ d => detail(d.detail) } on:hovered={ d => $hovered = d.detail } />
     </Svg>
   </LayerCake>
   <Drawer width="400px">
