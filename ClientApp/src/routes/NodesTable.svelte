9d59c5dbc73d8a5f98057766a06b7a83de5bd02b
DTTerastar <DT@Terastar.biz>
2023-01-14 14:33:57 -0500

    Node table kinda works

--- ClientApp/src/routes/NodesTable.svelte
+++ ClientApp/src/routes/NodesTable.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
-    import {
-	createDataTableStore,
-	dataTableHandler,
-	tableInteraction,
-	tableA11y
-} from '@skeletonlabs/skeleton';
+  import {createDataTableStore,	dataTableHandler,	tableInteraction,	tableA11y} from '@skeletonlabs/skeleton';
+  import { config, devices } from '../lib/stores';
+  import type { Node, Device } from '../lib/types';
+
+  let nodes: Node[] | undefined;
+  $: nodes = $config?.nodes;
 </script>
 
-
 <div class="table-container">
-	<table class="table table-hover">
+  {#if nodes }
+  <table class="table table-hover">
 		<thead>
 			<tr>
-				<th>Heading 1</th>
+				<th>Name</th>
 			</tr>
 		</thead>
 		<tbody>
+      {#each nodes as n}
 			<tr>
-				<td>Cell 1</td>
+				<td>{@html n.name}</td>
 			</tr>
+      {/each}
 		</tbody>
 	</table>
+  {/if}
 </div>
 
 <style>
+  .table-container { padding: 10px; }
 </style>
