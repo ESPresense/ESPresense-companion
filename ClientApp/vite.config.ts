961cd781963254f8c27e17095703fcf7aeb3cc40
Darrell <DT@Terastar.biz>
2023-03-15 06:41:30 -0400

    Fix minify (#82)

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -2,19 +2,23 @@ import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	"server": {
-		port: 44490,
-		proxy: {
-			'/api': 'http://localhost:5279',
-			'/ws': {
-				target: 'ws://localhost:5279',
-				ws: true
-			}
-		},
-		host: true
-	},
+  build: {
+    minify: false,
+  },
 
-	plugins: [
-		sveltekit()
-	]
+  server: {
+    port: 44490,
+    proxy: {
+      '/api': 'http://localhost:5279',
+      '/ws': {
+        target: 'ws://localhost:5279',
+        ws: true
+      }
+    },
+    host: true
+  },
+
+  plugins: [
+    sveltekit()
+  ]
 });
