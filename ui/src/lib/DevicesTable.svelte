4200f447311c96d4b50d1b2a76319addce88925f
DTTerastar <DT@Terastar.biz>
2023-06-30 16:36:59 -0400

    Make devices table sortable

--- ui/src/lib/DevicesTable.svelte
+++ ui/src/lib/DevicesTable.svelte
@@ -3,6 +3,7 @@
 	import type { Device } from '$lib/types';
 	import { createEventDispatcher } from 'svelte';
 	import SvelteTable from 'svelte-table';
+  import ago from 's-ago';
 
 	let dispatcher = createEventDispatcher();
 	let selected = '';
@@ -13,16 +14,16 @@
 	}
 
 	let columns = [
-		{ title: 'Id', value: (d) => d.id },
-		{ title: 'Name', value: (d) => d.name },
-		{ title: 'Room / Floor', value: (d) => d.room?.name ?? d.floor?.name ?? 'n/a' },
-		{ title: 'X', value: (d) => d.location?.x },
-		{ title: 'Y', value: (d) => d.location?.y },
-		{ title: 'Z', value: (d) => d.location?.z },
-		{ title: 'Fixes', value: (d) => d.fixes ?? 'n/a' },
-		{ title: 'Scale', value: (d) => d.scale ?? 'n/a' },
-		{ title: 'Confidence', value: (d) => d.confidence ?? 'n/a' },
-		{ title: 'LastHit', value: (d) => d.lastHit ?? 'n/a' }
+		{ key: "id", title: 'Id', value: (d) => d.id, sortable: true, defaultSort: true },
+		{ key: "name", title: 'Name', value: (d) => d.name, sortable: true },
+		{ key: "room", title: 'Room / Floor', value: (d) => d.room?.name ?? d.floor?.name ?? 'n/a', sortable: true },
+		{ key:"location.x", title: 'X', value: (d) => d.location?.x, sortable: true},
+		{ key: "location.y", title: 'Y', value: (d) => d.location?.y, sortable: true },
+		{ key: "location.z", title: 'Z', value: (d) => d.location?.z, sortable: true },
+		{ key: "fixes", title: 'Fixes', value: (d) => d.fixes ?? 'n/a',sortable: true },
+		{ key: "scale", title: 'Scale', value: (d) => d.scale?.toFixed(3) ?? 'n/a', sortable: true },
+		{ key: "confidence", title: 'Confidence', value: (d) => d.confidence ?? 'n/a', sortable: true },
+		{ key: "lastHit",  title: 'LastHit', value: (d) => (d.lastHit ?? "") == "" ? "n/a" : ago(new Date(d.lastHit)) ?? 'n/a', sortable: true }
 	];
 
 	function onRowClick(e) {
@@ -32,7 +33,7 @@
 
 <div class="table-container">
 	{#if $devices}
-		<SvelteTable {columns} classNameTable="table table-hover" rows={$devices} on:clickRow={onRowClick} />
+		<SvelteTable {columns} classNameTable="table table-hover table-compact" rows={$devices} on:clickRow={onRowClick} sortBy="id" />
 	{/if}
 </div>
 
