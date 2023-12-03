cc8ff3f754e3be7c02315ae9203dc2eac42f7168
DTTerastar <DT@Terastar.biz>
2023-12-03 05:13:52 -0500

    Add variance to radar

---
+++ src/Locators/MLEMultilateralizer.cs
@@ -0,0 +1,122 @@
+﻿using ESPresense.Extensions;
+using ESPresense.Models;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Optimization;
+using MathNet.Spatial.Euclidean;
+using Serilog;
+
+namespace ESPresense.Locators;
+
+public class MLEMultilateralizer(Device device, Floor floor, State state) : ILocate
+{
+    public bool Locate(Scenario scenario)
+    {
+        double Error(IList<double> x, DeviceNode dn)
+        {
+            double expectedDistance = Math.Sqrt(Math.Pow(dn.Node!.Location.X - x[0], 2) +
+                                                Math.Pow(dn.Node!.Location.Y - x[1], 2) +
+                                                Math.Pow(dn.Node!.Location.Z - x[2], 2));
+            double error = dn.Distance - expectedDistance;
+            double fallbackVariance = 1e-6; // This should be a small positive number
+            double variance = dn.Variance ?? fallbackVariance;
+            variance = Math.Max(variance, fallbackVariance); // Ensure variance is never zero
+
+            return error * error / (2 * variance);
+        }
+
+        var confidence = scenario.Confidence;
+
+        var nodes = device.Nodes.Values.Where(a => a.Current && (a.Node?.Floors?.Contains(floor) ?? false)).OrderBy(a => a.Distance).ToArray();
+        var pos = nodes.Select(a => a.Node!.Location).ToArray();
+
+        scenario.Minimum = nodes.Min(a => (double?)a.Distance);
+        scenario.LastHit = nodes.Max(a => a.LastHit);
+        scenario.Fixes = pos.Length;
+
+        if (pos.Length <= 1)
+        {
+            scenario.Room = null;
+            scenario.Confidence = 0;
+            scenario.Error = null;
+            scenario.Floor = null;
+            return false;
+        }
+
+        scenario.Floor = floor;
+
+        var guess = confidence < 5
+            ? Point3D.MidPoint(pos[0], pos[1])
+            : scenario.Location;
+        try
+        {
+            if (pos.Length < 3 || floor.Bounds == null)
+            {
+                confidence = 1;
+                scenario.Location = guess;
+            }
+            else
+            {
+                var lowerBound = Vector<double>.Build.DenseOfArray(new[] { floor.Bounds[0].X, floor.Bounds[0].Y, floor.Bounds[0].Z, 0.5 });
+                var upperBound = Vector<double>.Build.DenseOfArray(new[] { floor.Bounds[1].X, floor.Bounds[1].Y, floor.Bounds[1].Z, 1.5 });
+                var obj = ObjectiveFunction.Value(
+                    x =>
+                    {
+                        var distanceFromBoundingBox = lowerBound.Subtract(x)
+                            .PointwiseMaximum(x.Subtract(upperBound))
+                            .PointwiseMaximum(0)
+                            .L2Norm();
+                        return (distanceFromBoundingBox > 0 ? Math.Pow(5, 1 + distanceFromBoundingBox) : 0) + Math.Pow(5 * (1 - x[3]), 2) + nodes
+                            .Select((dn, i) => new { err = Error(x, dn), weight = state?.Weighting?.Get(i, nodes.Length) ?? 1.0 })
+                            .Average(a => a.weight * Math.Pow(a.err, 2));
+                    });
+
+                var initialGuess = Vector<double>.Build.DenseOfArray(new[]
+                {
+                    Math.Max(floor.Bounds[0].X, Math.Min(floor.Bounds[1].X, guess.X)),
+                    Math.Max(floor.Bounds[0].Y, Math.Min(floor.Bounds[1].Y, guess.Y)),
+                    Math.Max(floor.Bounds[0].Z, Math.Min(floor.Bounds[1].Z, guess.Z)),
+                    scenario.Scale ?? 1.0
+                });
+                var centroid = Point3D.Centroid(nodes.Select(n => n.Node!.Location).Take(3)).ToVector();
+                var vectorToCentroid = centroid.Subtract(initialGuess.SubVector(0, 3)).Normalize(2);
+                var scaleDelta = 0.05 * initialGuess[3];
+                var initialPerturbation = Vector<double>.Build.DenseOfEnumerable(vectorToCentroid.Append(scaleDelta));
+                var solver = new NelderMeadSimplex(1e-7, 10000);
+                var result = solver.FindMinimum(obj, initialGuess, initialPerturbation);
+                var minimizingPoint = result.MinimizingPoint.PointwiseMinimum(upperBound).PointwiseMaximum(lowerBound);
+                scenario.Location = new Point3D(minimizingPoint[0], minimizingPoint[1], minimizingPoint[2]);
+                scenario.Scale = minimizingPoint[3];
+                scenario.Fixes = pos.Length;
+                scenario.Error = result.FunctionInfoAtMinimum.Value;
+                scenario.Iterations = result switch
+                {
+                    MinimizationWithLineSearchResult mwl =>
+                        mwl.IterationsWithNonTrivialLineSearch + mwl.TotalLineSearchIterations,
+                    _ => result.Iterations
+                };
+
+                scenario.ReasonForExit = result.ReasonForExit;
+                confidence = (int)Math.Min(100, Math.Max(10, 100.0 - (Math.Pow(scenario.Minimum ?? 1, 2) + Math.Pow(10 * (1 - (scenario.Scale ?? 1)), 2) + (scenario.Minimum + result.FunctionInfoAtMinimum.Value ?? 10.00))));
+            }
+        }
+        catch (MaximumIterationsException)
+        {
+            scenario.ReasonForExit = ExitCondition.ExceedIterations;
+            confidence = 1;
+            scenario.Location = guess;
+        }
+        catch (Exception ex)
+        {
+            confidence = 0;
+            scenario.Location = new Point3D();
+            Log.Error("Error finding location for {0}: {1}", device, ex.Message);
+        }
+
+        scenario.Confidence = confidence;
+
+        if (confidence <= 0) return false;
+        if (Math.Abs(scenario.Location.DistanceTo(scenario.LastLocation)) < 0.1) return false;
+        scenario.Room = floor.Rooms.Values.FirstOrDefault(a => a.Polygon?.EnclosesPoint(scenario.Location.ToPoint2D()) ?? false);
+        return true;
+    }
+}
--- src/ui/src/lib/NodeMarker.svelte
+++ src/ui/src/lib/NodeMarker.svelte
@@ -22,24 +22,43 @@
 
 	function radiusOnIntersectionCircle(d: Device | undefined, n: Node): number {
 		if (d == undefined) return 0;
-		var dr = d.nodes[n.id];
+		var dr = d.nodes[n.id]?.dist;
 		if (dr == undefined) return 0;
-		var nz = n.point[2];
-		var dz = d.location.z;
-		var heightDifference = dz - nz;
+		return dr;
+	}
 
-		// Check if the plane intersects the sphere
-		if (Math.abs(heightDifference) > dr) return 0;
-
-		var radius = Math.sqrt(Math.pow(dr, 2) - Math.pow(heightDifference, 2));
-		return radius;
-}
+	function errorBarLength(dr: number, variance: number): { x: number; y: number } {
+		return { x: variance, y: variance };
+	}
 </script>
 
+<defs>
+	{#if $r > 0 && radar && radar.nodes[n.id]}
+		<radialGradient id="variance-gradient-{n.id}" cx={$xScale(n.point[0])} cy={$yScale(n.point[1])} r={$xScale($r + radar.nodes[n.id]?.var)} gradientUnits="userSpaceOnUse">
+			<!-- Transparent stop at the center minus variance -->
+			<stop offset={($r - radar.nodes[n.id]?.var) / ($r + radar.nodes[n.id]?.var)} style="stop-color:{colors(n.id)}; stop-opacity:0" />
+			<!-- Opaque stop at the center -->
+			<stop offset={$r / ($r + radar.nodes[n.id]?.var)} style="stop-color:{colors(n.id)}; stop-opacity:1" />
+			<!-- Transparent stop at the center plus variance -->
+			<stop offset={($r + radar.nodes[n.id]?.var) / ($r + radar.nodes[n.id]?.var)} style="stop-color:{colors(n.id)}; stop-opacity:0" />
+		</radialGradient>
+	{/if}
+</defs>
+
 <path d="M{$xScale(n.point[0])},{$yScale(n.point[1])} m -5,0 5,-5 5,5 -5,5 z" fill={colors(n.id)} />
-<text x='{ $xScale(n.point[0])  + 7}' y='{ $yScale(n.point[1])  + 3.5}' fill='white' font-size='10px'>{n.name}</text>
-{#if $r > 0}
-	<ellipse cx='{ $xScale(n.point[0]) }' cy='{ $yScale(n.point[1]) }' fill={colors(n.id)} stroke={colors(n.id)} fill-opacity='0.1' rx='{Math.abs($xScale(0) - $xScale($r))}' ry='{Math.abs($yScale(0) - $yScale($r))}' />
-	<text x='{ $xScale(n.point[0] - $r/2)}' y='{ $yScale(n.point[1] + $r/2)}' fill={colors(n.id)} font-size='12px'>{ radar?.nodes[n.id] ?? "" }</text>
-	<text x='{ $xScale(n.point[0] + $r/2)}' y='{ $yScale(n.point[1] - $r/2)}' fill={colors(n.id)} font-size='12px'>{ radar?.nodes[n.id] ?? "" }</text>
+<text x={$xScale(n.point[0]) + 7} y={$yScale(n.point[1]) + 3.5} fill="white" font-size="10px">{n.name}</text>
+{#if $r > 0 && radar && radar.nodes[n.id]}
+	<ellipse cx={$xScale(n.point[0])} cy={$yScale(n.point[1])} fill={`url(#variance-gradient-${n.id})`} rx={Math.abs($xScale(0) - $xScale($r + radar.nodes[n.id]?.var))} ry={Math.abs($yScale(0) - $yScale($r + radar.nodes[n.id]?.var))} fill-opacity="0.5" />
+	<ellipse cx={$xScale(n.point[0])} cy={$yScale(n.point[1])} fill="none" stroke={colors(n.id)} rx={Math.abs($xScale(0) - $xScale($r))} ry={Math.abs($yScale(0) - $yScale($r))} stroke-width="2" />
+	<g>
+		<line x1={$xScale(n.point[0] - errorBarLength(radar.nodes[n.id]?.dist, radar.nodes[n.id]?.var).x)} y1={$yScale(n.point[1])} x2={$xScale(n.point[0] + errorBarLength(radar.nodes[n.id]?.dist, radar.nodes[n.id]?.var).x)} y2={$yScale(n.point[1])} stroke={colors(n.id)} stroke-width="2" />
+		<line x1={$xScale(n.point[0])} y1={$yScale(n.point[1] - errorBarLength(radar.nodes[n.id]?.dist, radar.nodes[n.id]?.var).y)} x2={$xScale(n.point[0])} y2={$yScale(n.point[1] + errorBarLength(radar.nodes[n.id]?.dist, radar.nodes[n.id]?.var).y)} stroke={colors(n.id)} stroke-width="2" />
+		<text x={$xScale(n.point[0])} y={$yScale(n.point[1]) + 15} fill="white" font-size="10px">
+			{#if radar.nodes[n.id]?.var !== null && radar.nodes[n.id]?.var !== undefined}
+				{radar.nodes[n.id]?.dist.toFixed(2) ?? 'N/A'} ± {radar.nodes[n.id]?.var.toFixed(2)}
+			{:else}
+				{radar.nodes[n.id]?.dist.toFixed(2) ?? 'N/A'}
+			{/if}
+		</text>
+	</g>
 {/if}
