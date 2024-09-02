13af6d8247802f46dd1fc79d89f6ab6d304749ca
DTTerastar <DT@Terastar.biz>
2024-09-02 18:27:37 -0400

    Add openhands

---
+++ agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export WORKSPACE_BASE=$(pwd)
+
+sudo docker run \
+    -it \
+    --pull=always \
+    --add-host host.docker.internal:host-gateway \
+    -e SANDBOX_USER_ID=$(id -u) \
+    -e LLM_OLLAMA_BASE_URL="http://host.docker.internal:11434" \
+    -e WORKSPACE_MOUNT_PATH=$WORKSPACE_BASE \
+    -v $WORKSPACE_BASE:/opt/workspace_base \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -p 3000:3000 \
+    --name openhands-app-$(date +%Y%m%d%H%M%S) \
+    ghcr.io/all-hands-ai/openhands:0.9
