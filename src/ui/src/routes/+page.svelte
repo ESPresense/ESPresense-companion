66ddd1030fd04c374362451213a0ccd7767edcd7
Darrell <DT@Terastar.biz>
2024-11-16 20:25:49 -0500

    Background upload (#841)
    * Background Upload w/ rotate!

--- src/ui/src/routes/+page.svelte
+++ src/ui/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Map from '$lib/Map.svelte';
 	import FloorTabs from '$lib/FloorTabs.svelte';
+	import BackgroundUpload from '$lib/BackgroundUpload.svelte';
 	import { detail } from '$lib/urls';
 
 	export let floorId: string | null = null;
@@ -12,5 +13,6 @@
 
 <div class="w-full h-full bg-slate-900">
 	<FloorTabs bind:floorId />
+	<BackgroundUpload />
 	<Map on:selected={(d) => detail(d.detail)} bind:floorId />
 </div>
