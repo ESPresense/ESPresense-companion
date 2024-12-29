7497073f753900611820e0ace0a45d20f2d34bd3
Darrell <DT@Terastar.biz>
2024-12-29 17:01:11 -0500

    Update eslint (#926)

---
+++ src/ui/eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import globals from "globals";
+import tsEslint from "typescript-eslint";
+import vitest from "eslint-plugin-vitest";
+import playwright from "eslint-plugin-playwright";
+
+export default [
+  js.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/recommended"],
+  eslintConfigPrettier,
+  {
+    ...playwright.configs["flat/playwright"],
+    files: ["tests/**"]
+  },
+  vitest.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/prettier"],
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.node, ...globals.browser },
+      parserOptions: {
+        extraFileExtensions: [".svelte"]
+      }
+    }
+  },
+  {
+    ignores: [
+      ".svelte-kit",
+      "build",
+      "package",
+      "coverage",
+      "node_modules",
+      "playwright.config.js"
+    ]
+  }
+];
