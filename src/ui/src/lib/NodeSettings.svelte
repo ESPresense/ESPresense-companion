9fcc7d00126680865584ee2bbdb206212e163b4d
DTTerastar <DT@Terastar.biz>
2025-03-30 23:46:32 -0400

    Misc tweaks

--- src/ui/src/lib/NodeSettings.svelte
+++ src/ui/src/lib/NodeSettings.svelte
@@ -15,19 +15,19 @@
 </label>
 <label>
   <span>Absorption</span>
-  <input class="input" type="text" placeholder="" bind:value={settings.calibration.absorption} />
+  <input class="input" type="number" bind:value={settings.calibration.absorption} />
 </label>
 <label>
   <span>Rx Adj Rssi</span>
-  <input class="input" type="text" placeholder="" bind:value={settings.calibration.rxAdjRssi} />
+  <input class="input" type="number" bind:value={settings.calibration.rxAdjRssi} />
 </label>
 <label>
   <span>Tx Ref Rssi</span>
-  <input class="input" type="text" placeholder="" bind:value={settings.calibration.txRefRssi} />
+  <input class="input" type="number" bind:value={settings.calibration.txRefRssi} />
 </label>
 <label>
   <span>Max Distance</span>
-  <input class="input" type="text" placeholder="" bind:value={settings.filtering.maxDistance} />
+  <input class="input" type="number" bind:value={settings.filtering.maxDistance} />
 </label>
 {:else}
   <div class="text-center text-token">Node settings not available.</div>
