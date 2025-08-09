5f23f11c48d58c919aa5e91cda9061c8edb59bb6
Darrell <DT@Terastar.biz>
2025-08-09 12:31:13 -0400

    chore: restore tailwind purge plugin

--- src/ui/vite.config.ts
+++ src/ui/vite.config.ts
@@ -1,8 +1,9 @@
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), purgeCss()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
