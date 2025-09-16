4bf73ebec201418a2563f2c9b628c33b18ac8a3a
Darrell <DT@Terastar.biz>
2025-09-16 18:33:39 -0400

    feat(ui): add Chrome devtools integration

--- src/ui/vite.config.ts
+++ src/ui/vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [devtoolsJson(), tailwindcss(), sveltekit()],
 
 
 	build: {
